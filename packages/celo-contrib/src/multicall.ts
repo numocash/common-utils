a76b100c5c38711ad290eb0a9d079dbee7812fcd
Kyle Scott <kyscott@umich.edu>
2022-04-15 19:20:32 -0400

    multicall fix

--- packages/celo-contrib/src/multicall.ts
+++ packages/celo-contrib/src/multicall.ts
@@ -1,6 +1,6 @@
 import { ChainId } from "./network";
 
-export const BankAddress: { [chainId in ChainId]: string } = {
+export const Multicall: { [chainId in ChainId]: string } = {
   [ChainId.Mainnet]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
   [ChainId.Alfajores]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
   [ChainId.Baklava]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
