dd39e72f0533760d69ab68c6a0aee5c49f3f0da6
Kyle Scott <kyscott@umich.edu>
2022-04-15 15:47:09 -0400

    celo-contrib refactor

---
+++ packages/celo-contrib/src/multicall.ts
@@ -0,0 +1,7 @@
+import { ChainId } from "./network";
+
+export const BankAddress: { [chainId in ChainId]: string } = {
+  [ChainId.Mainnet]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
+  [ChainId.Alfajores]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
+  [ChainId.Baklava]: "0x9aac9048fC8139667D6a2597B902865bfdc225d3",
+};
