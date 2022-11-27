fc3ec0a1cad0ea47710877d3bde46e2ec6c97306
Kyle Scott <kyscott@umich.edu>
2022-11-26 20:39:48 -0500

    fix celo-contrib dependency

--- packages/use-ethers/src/multicall.ts
+++ packages/use-ethers/src/multicall.ts
@@ -1,17 +1,16 @@
-import type { ChainId } from "@dahlia-labs/celo-contrib";
-import { Multicall as MulticallAddress } from "@dahlia-labs/celo-contrib";
 import chunk from "lodash/chunk";
 
 import MULTICALL_ABI from "./abis/multicall2.json";
+import { MulticallAddress } from "./constants";
 import { getContract } from "./contracts";
 import type { Multicall2 } from "./generated";
-import type { Call, Multicall, ProviderOrSigner } from "./types";
+import type { Call, chainID, Multicall, ProviderOrSigner } from "./types";
 
 export const getMulticall = (
-  chainID: ChainId,
-  provider: ProviderOrSigner
+  provider: ProviderOrSigner,
+  chain: keyof typeof chainID
 ): Multicall2 =>
-  getContract(MulticallAddress[chainID], MULTICALL_ABI, provider) as Multicall2;
+  getContract(MulticallAddress[chain], MULTICALL_ABI, provider) as Multicall2;
 
 export async function fetchMulticalls<T extends unknown[]>(
   multicalls: readonly [...{ [I in keyof T]: Multicall<T[I]> }],
