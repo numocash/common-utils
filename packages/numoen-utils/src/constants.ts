5a91f5be4eb969f8e5777c95d28dd8e68b3d7333
Kyle Scott <kyscott@umich.edu>
2022-11-30 14:27:51 -0500

    update numoen contracts

--- packages/numoen-utils/src/constants.ts
+++ packages/numoen-utils/src/constants.ts
@@ -3,11 +3,11 @@ import type { ChainsV1 } from "./types";
 type AddressMap = { [chain in ChainsV1]: string };
 
 export const FACTORY: AddressMap = {
-  goerli: "0x926DE2040e0f0DCC6524d3cFADf25A59A8f16Ee7",
+  goerli: "0x010797814E619634c0A6bbaA9FaCa48FBD0D3E33",
 };
 export const LIQUIDITYMANAGER: AddressMap = {
-  goerli: "0x1b8dd4f91b5d8b814fbc03b75171be9792b4ab69",
+  goerli: "0x82d4D8a3609F8C5d19b59339A75E2a25AfC3e564",
 };
 export const LENDGINEROUTER: AddressMap = {
-  goerli: "0x342de0bee2420cb3cc4934fdb4d2f402959f1120",
+  goerli: "0x0a0E6120228f521f38b16dD12aA6CD859c307bC4",
 };
